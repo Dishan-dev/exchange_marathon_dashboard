f5e711fe5bf60f4f2197e35ef74fc857106ba3a2
H.M.D.B. Bandara <dshnz29@gmail.com>
2026-03-28 10:03:32 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans text-[#F7F7F8] bg-black flex flex-col min-h-screen`}
       >
